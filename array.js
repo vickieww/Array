18336455db9bae2be08b5f6666281d0e14c204d7
Vitoria <gabrieli.vitoria@alunoescolassociais.org.br>
2023-08-08 08:24:21 -0300

    termina projeto de agenda com estilização de css

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 var agenda = [];
 var dataAgenda = [];
 
-function salvarUser() {
+function salvarC() {
     let nomeUser = document.getElementById("nomeUser").value;
     let dataC = document.getElementById("dataC").value;
     if (nomeUser) {
@@ -15,27 +15,29 @@ function salvarUser() {
 
 }
 
-//FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
+//FUNÇÃO PARA SALVAR COMPROMISSO E DATA DO COMPROMISSO
 function criarLista() {
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
 
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Compromissos</th><th>Data</th></tr>";
     for (let i = 0; i <= (agenda.length - 1); i++) {
+
         tabela += "<tr><td>" + agenda[i] + ' ' + dataAgenda[i]
+
         + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+
         document.getElementById('tabela').innerHTML = tabela;
     }
+
 }
 
-//FUNÇÃO PARA EDITAR NOME
-
+//FUNÇÃO PARA EDITAR COMPROMISSO
 function editar(i) {
     document.getElementById('nomeUser').value = agenda[(i - 1)];
-    dadosLista.splice(agenda[(i - 1)], 1);
+    agenda.splice(dataAgenda[(i - 1)], 1);
+
 }
 
-// FUNÇÃO PARA EXCLUIR NOME
-
+// FUNÇÃO PARA EXCLUIR COMPROMISSO
 function excluir(i) {
     agenda.splice((i - 1), 1);
-    document.getElementById('tabela').deleteRow(i);
-}
+    document.getElementById('tabela').deleteRow(i)}
